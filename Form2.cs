d883aabcee2e16884cc0d780de9b1bb33cbbc44b
crnvmrk <102481632+crnvmrk@users.noreply.github.com>
2022-06-03 06:48:06 +0300

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Car_Dealership
+{
+    enum RowState
+    {
+        Existed,
+        New,
+        Modified,
+        ModifiedNew,
+        Deleted
+
+    }
+    public partial class Form2 : Form
+    {
+        Database database = new Database();
+        int selectedRow;
+
+        public Form2()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        private void CreateColumns()
+        {
+            dataGridView1.Columns.Add("id_user", "id");
+            dataGridView1.Columns.Add("login_user", "Логин");
+            dataGridView1.Columns.Add("passsword_user", "Пароль");
+            dataGridView1.Columns.Add("role", "Роль");
+            dataGridView1.Columns.Add("surname", "Фамилия");
+            dataGridView1.Columns.Add("name", "Имя");
+            dataGridView1.Columns.Add("patronymic_user", "Отчество");
+            dataGridView1.Columns.Add("IsNew", String.Empty);
+        }
+        private void ReadSingleRow(DataGridView dgw, IDataRecord record)
+        {
+            dgw.Rows.Add(record.GetInt32(0), record.GetString(1), record.GetString(2), record.GetString(3), record.GetString(4), record.GetString(5), record.GetString(6), RowState.ModifiedNew);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            CreateColumns();
+            RefreshDataGrid(dataGridView1);
+
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[selectedRow];
+
+                textBox8.Text = row.Cells[0].Value.ToString();
+                textBox9.Text = row.Cells[1].Value.ToString();
+                textBox11.Text = row.Cells[2].Value.ToString();
+                textBox10.Text = row.Cells[3].Value.ToString();
+                textBox13.Text = row.Cells[4].Value.ToString();
+                textBox12.Text = row.Cells[5].Value.ToString();
+                textBox14.Text = row.Cells[6].Value.ToString();
+            }
+        }
+        private void RefreshDataGrid(DataGridView dgw)
+        {
+            dgw.Rows.Clear();
+            string queryString = $"select* from reg";
+            SqlCommand command = new SqlCommand(queryString, database.getConnection());
+            database.openConnection();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                ReadSingleRow(dgw, reader);
+            }
+            reader.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            database.openConnection();
+            var login = textBox2.Text;
+            var password = textBox3.Text;
+            var role = textBox4.Text;
+            var surname = textBox5.Text;
+            var name = textBox6.Text;
+            var patronymic = textBox7.Text;
+           
+                var addQuery = $"insert into reg (login_user,passsword_user,role_user,surname_user,name_user,patronymic_user) values ('{login}','{password}','{role}','{surname}','{name}','{patronymic}')";
+                var command = new SqlCommand(addQuery, database.getConnection());
+                command.ExecuteNonQuery();
+                MessageBox.Show("Запись успешно создана!", "Успех!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+           
+            
+            database.CloseConnection();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox8.Text = "";
+        }
+    }
+    }
+
