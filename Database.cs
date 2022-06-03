8d5a5d6321bf95eff344d2df2bf6590ed1cd9c83
crnvmrk <102481632+crnvmrk@users.noreply.github.com>
2022-06-03 06:42:30 +0300

    Add files via upload

---
+++ Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Car_Dealership
+{
+    internal class Database
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=LAPTOP-LM2JUQ16\SQLEXPRESS;Initial Catalog=AS;Integrated Security=True");
+        public void openConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
